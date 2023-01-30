cca4c53a99f82a25a0596fa53a1187019116a90e
IAmNameless1 <md.firoz95@gmail.com>
2023-01-30 10:52:59 +0530

    update

--- src/components/social.js
+++ src/components/social.js
@@ -8,7 +8,7 @@ export default function Social() {
     return (
         
         <ul className='w-fit social m-2'>
-        <List className="social_media" href="wa.link/jro7i7" name={<IoLogoWhatsapp/>}/>
+        <List className="social_media" href="https://wa.link/gkj996" name={<IoLogoWhatsapp/>}/>
         <List className="social_media" href="https://www.linkedin.com/in/mohammad-firoz-73597b12a" name={<AiFillLinkedin/>}/>
         <List className="social_media" href="https://www.instagram.com/firoz95ahmad/" name={<BsInstagram/>}/>
         <List className="social_media" href="https://github.com/IAmNameless1" name={<BsGithub/>}/>
