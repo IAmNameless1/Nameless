74b235df86ba7ac9f0220c93f5a29d5eae69c5cb
IAmNameless1 <md.firoz95@gmail.com>
2023-01-31 00:17:42 +0530

    update

--- src/container/Recentworks.js
+++ src/container/Recentworks.js
@@ -12,18 +12,18 @@ export default function Projectrow() {
         <h1 className='text-5xl h-fit ml-12 pl-12'><span className='deco'>Rece</span>nt   Projects</h1><div className="span w-fit"></div>
         <div className="grid lg:grid-col-3 p-10 pb-0">
             <div className="lg:col-start-1 lg:col-span-1">
-            <Project p="https://firdosh008.github.io/hotle-/" s="https://github.com/firdosh008/hotle-" src={P1} name="SRA Company Hotle"/>
+            <Project p="https://docs.google.com/document/d/1PRMe2xUnJFRKZ1oLksrBtr66oR0-MtbX/edit?usp=sharing&ouid=110944947663564822368&rtpof=true&sd=true" src={P1} name="SMM Campaign Strategy"/>
             </div>
              <div className="lg:col-start-2 lg:col-end-3">
-             <Project p="https://avsfffgsfgwt43.my.canva.site/"/>
+             <Project p="https://docs.google.com/spreadsheets/d/1tMxX86firj2-wrMCsGvgICo4CRPn2Ocm/edit?usp=sharing&ouid=110944947663564822368&rtpof=true&sd=true" src={P3} name="SMM Content Clanedar"/>
              </div>
              <div className="lg:col-start-3 lg:col-end-4">
-             <Project p="https://firdosh008.github.io/dicegame/" s="https://github.com/firdosh008/dicegame" src={P3} name="Dice Toss Game"/>
+             <Project p="https://drive.google.com/file/d/1U-SU5bA6yHAMmIXGQQHOHWXijZTDjFa6/view?usp=sharing" s="https://github.com/firdosh008/dicegame" src={P3} name="Campaign Plan For A Budget"/>
              </div> 
          </div>
          <div className="grid lg:grid-col-3 p-10 pb-0">
             <div className="lg:col-start-1 lg:col-span-1">
-            <Project p="https://firdosh008.github.io/simon-game/" s="https://github.com/firdosh008/simon-game" src={P4} name="Simon Game"/>
+            <Project p="https://firdosh008.github.io/simon-game/" s="https://github.com/firdosh008/simon-game" src={P4} name="Design a Website Structure"/>
             </div>
              <div className="lg:col-start-2 lg:col-end-3">
              <Project p="" s="" src={P5} name="Restraunt Website"/>
